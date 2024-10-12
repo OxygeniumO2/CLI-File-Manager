aa5c83e3d49953ebd163971687040f98aaa32d1a
unknown <feikomilcezzz2@gmail.com>
2024-10-12 14:20:50 +0300

    fix: compress decompress

--- modules/fileSystem/compressDecompress.js
+++ modules/fileSystem/compressDecompress.js
@@ -4,14 +4,28 @@ import { pipeline } from 'node:stream/promises';
 import { COMMAND_FS } from '../../utils/constants.js';
 import { ERROR } from '../../utils/constants.js';
 import fs from 'node:fs';
+import path from 'node:path';
 
 const compressDecompress = async (command, args) => {
   try {
     const currentArgs = args;
     const currentCommand = command;
 
-    const [from, to] = pathResolver(currentArgs);
+    let [from, to] = pathResolver(currentArgs);
+
     const brotli = currentCommand === COMMAND_FS.compress ? BrotliCompress() : BrotliDecompress();
+
+    let fileName = path.basename(from);
+
+    if (currentCommand === COMMAND_FS.compress) {
+      to = path.join(to, `${fileName}.br`);
+    } else {
+      if (path.extname(fileName) === '.br') {
+        to = path.join(to, fileName.slice(0, -3));
+      } else {
+        to = path.join(to, fileName);
+      }
+    }
 
     const fromStream = fs.createReadStream(from);
     const toStream = fs.createWriteStream(to);
